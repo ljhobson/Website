682e57bf680274a88bb6a6df21b9e9d3d5d8b993
ljhobson <louishobson5@gmail.com>
2023-06-06 01:38:32 +1200

    bch

---
+++ pacman/script.js
@@ -0,0 +1,358 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+ctx.imageSmoothingEnabled = false;
+
+var img = {};
+img.maze = new Image();
+img.maze.src = "maze.png";
+
+img.pacman = new Image();
+img.pacman.src = "sprites.png";
+
+img.ghost = new Image();
+img.ghost.src = "sprites.png";
+
+
+var fps = 10;
+setInterval(update, 1000/fps);
+
+var mapWidth = 28;
+var mapHeight = 31;
+var map = [
+	1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+	1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+	1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1,
+	1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1,
+	1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1,
+	1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+	1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1,
+	1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1,
+	1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1,
+	1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+	1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+	1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+	1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+	1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+	1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+	1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+	1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+	1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+	1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+	1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1,
+	1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1,
+	1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1,
+	1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1,
+	1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1,
+	1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1,
+	1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1,
+	1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1,
+	1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+	1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+];
+
+var dots = [
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 
+0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 
+0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 
+0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 
+0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 
+0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 
+0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 
+0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 
+0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 
+0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 
+0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 
+0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 
+0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 
+0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 
+0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 
+0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 
+0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 
+0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 
+0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+];
+for (var i = 0; i < map.length; i++) {
+	//dots.push(1-map[i]);
+}
+
+function rand(a, b) {
+	return(a+Math.round((b-a)*Math.random()));
+}
+
+var scale = 16;
+
+var WIDTH = mapWidth*scale;
+var HEIGHT = mapHeight*scale;
+
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
+
+var playerX = 13;
+var playerY = 17;
+var playerDirection = 3;
+
+var controls = [38, 39, 40, 37];
+var direction = 3;
+
+var ghosts = [{x: 13, y: 11, d: rand(0, 3)}, {x: 13, y: 11, d: rand(0, 3)}, {x: 13, y: 11, d: rand(0, 3)}, {x: 13, y: 11, d: rand(0, 3)}];
+document.onkeydown = function(event) {
+	if (controls.includes(event.keyCode)) {
+		direction = controls.indexOf(event.keyCode);
+	}
+}
+
+var cherry = false;
+function resetGame() {
+	alive = true;
+	counter = 0;
+	score = 0;
+	playerX = 13;
+	playerY = 17;
+	playerDirection = 3;
+	controls = [38, 39, 40, 37];
+	direction = 3;
+	ghosts = [{x: 13, y: 11, d: rand(0, 3)}, {x: 13, y: 11, d: rand(0, 3)}, {x: 13, y: 11, d: rand(0, 3)}, {x: 13, y: 11, d: rand(0, 3)}];
+	dots = [
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 
+0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 
+0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 
+0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 
+0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 
+0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 
+0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 
+0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 
+0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 
+0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 
+0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 
+0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 
+0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 
+0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 
+0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 
+0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 
+0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 
+0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 
+0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+];
+	points = [];
+	cherry = false;
+	cherryTime = 0;
+}
+
+var alive = true;
+var counter = 0;
+var score = 0;
+function update() {
+	if (!dots.includes(1)) {
+		alert("You Win!\nScore: " + score + "\nPlay Again?");
+		resetGame();
+	} else {
+		if (alive) {
+			counter++;
+			updateCherry();
+			movePlayer();
+			eatDots();
+			updatePoints();
+			updateGhosts();
+			render();
+		} else {
+			alert("You Lost\nScore: " + score + "\nClick OK to Restart");
+			resetGame();
+		}
+	}
+}
+
+var cherryTime = 0;
+function updateCherry() {
+	cherryTime++;
+	if (cherry) {
+		if (playerX === 13 && playerY === 17) {
+			cherry = false;
+			score += 500;
+			points.push({num: "+500", life: 0, x: scale*playerX+rand(-scale, scale), y: scale*playerY+rand(-scale, scale)});
+			cherryTime = 0;
+		}
+		if (counter % rand(20, 50) === 0 && cherryTime > rand(50, 200)) {
+			cherry = false;
+			cherryTime = 0;
+		}
+	} else {
+		if (counter % rand(20, 50) === 0 && cherryTime > rand(100, 200)) {
+			cherry = true;
+			cherryTime = 0;
+		}
+	}
+}
+
+function updateGhosts() {
+	var options;
+	for (var i = 0; i < ghosts.length; i++) {
+		if ((ghosts[i].x === playerX) && (ghosts[i].y === playerY)) {
+			alive = false;
+		}
+		//Can turn?
+		options = [];
+		for (var j = 0; j < 4; j++) {
+			if (j !== ghosts[i].d && j !== (ghosts[i].d+2)%4) {
+				newPX = ghosts[i].x - (j)%2 * (j-2);
+				newPY = ghosts[i].y + (j+1)%2 * (j-1);
+				if (map[newPY*mapWidth + newPX] === 0) {
+					options.push(j);
+				}
+			}
+		}
+		if (options.length > 0) {
+			ghosts[i].d = options[rand(0, options.length-1)];
+		}
+		
+		//Can move forward?
+		newPX = ghosts[i].x - (ghosts[i].d)%2 * (ghosts[i].d-2);
+		newPY = ghosts[i].y + (ghosts[i].d+1)%2 * (ghosts[i].d-1);
+		if (newPX < 0) {
+			newPX = mapWidth-1;
+		} else if (newPX >= mapWidth) {
+			newPX = 0;
+		}
+		if (map[newPY*mapWidth + newPX] === 0) {
+			ghosts[i].x = newPX;
+			ghosts[i].y = newPY;
+		}
+		if ((ghosts[i].x === playerX) && (ghosts[i].y === playerY)) {
+			alive = false;
+		}
+	}
+}
+
+var newPX;
+var newPY;
+function movePlayer() {
+	//Can turn?
+	newPX = playerX - (direction)%2 * (direction-2);
+	newPY = playerY + (direction+1)%2 * (direction-1);
+	if (map[newPY*mapWidth + newPX] === 0) {
+		playerDirection = direction;
+	}
+	//Can move forward?
+	newPX = playerX - (playerDirection)%2 * (playerDirection-2);
+	newPY = playerY + (playerDirection+1)%2 * (playerDirection-1);
+	if (newPX < 0) {
+		newPX = mapWidth-1;
+	} else if (newPX >= mapWidth) {
+		newPX = 0;
+	}
+	if (map[newPY*mapWidth + newPX] === 0) {
+		playerX = newPX;
+		playerY = newPY;
+	}
+}
+
+var points = [];
+function eatDots() {
+	if (dots[playerY*mapWidth + playerX] === 1) {
+		dots[playerY*mapWidth + playerX] = 0;
+		score += 10;
+		points.push({num: "+10", life: 0, x: scale*playerX+rand(-scale, scale), y: scale*playerY+rand(-scale, scale)});
+	}
+}
+
+function updatePoints() {
+	for (var i = 0; i < points.length; i++) {
+		points[i].life++;
+		points[i].y -= 3*(5-points[i].life);
+		if (points[i].life > 5) {
+			points.splice(i, 1);
+			i--;
+		}
+	}
+}
+
+function render() {
+	ctx.clearRect(0, 0, WIDTH, HEIGHT);
+	
+	renderMap();
+	
+	renderDots();
+	
+	renderCherry();
+	
+	renderGhosts();
+	/*
+	ctx.fillStyle = "#ffff00";
+	ctx.fillRect(scale*playerX, scale*playerY, scale*1, scale*1);
+	*/
+	
+	ctx.drawImage(img.pacman, 229 + 16*(counter%3), 16*(((playerDirection-1)/2) * (playerDirection%2) + (2+playerDirection/2)*(1-(playerDirection%2))), 16, 16, playerX*scale - scale/2, playerY*scale - scale/2, 2*scale, 2*scale);
+	
+	renderPoints();
+}
+
+function renderCherry() {
+	if (cherry) {
+		ctx.drawImage(img.pacman, 229 + 16*3, 16*3, 16, 16, 13*scale - scale/2, 17*scale - scale/2, 2*scale, 2*scale);
+	}
+}
+
+function renderPoints() {
+	for (var i = 0; i < points.length; i++) {
+		ctx.font = String(4*(5-points[i].life)) + "px Arial";
+		ctx.fillText(points[i].num, points[i].x, points[i].y);
+	}
+}
+
+function renderGhosts() {
+	for (var i = 0; i < ghosts.length; i++) {
+		ctx.drawImage(img.ghost, 229 + 16*(counter%2) + (2*(16*(((ghosts[i].d-1)/2) * (ghosts[i].d%2) + (2+ghosts[i].d/2)*(1-(ghosts[i].d%2))))), 16*(4+i), 16, 16, scale*ghosts[i].x-scale/2, scale*ghosts[i].y-scale/2, 2*scale, 2*scale);
+		//ctx.font = "20px Arial";
+		//ctx.fillText(ghosts[i].d, ghosts[i].x*scale, ghosts[i].y*scale-8);
+	}
+}
+
+function renderMap() {
+	/*
+	for (var i = 0; i < map.length; i++) {
+		if (map[i] === 1) {
+			ctx.fillStyle = "#0000ff";
+		} else {
+			ctx.fillStyle = "#000000";
+		}
+		ctx.fillRect(i%mapWidth*scale, Math.floor(i/mapWidth)*scale, scale, scale);
+	}
+	*/
+	ctx.drawImage(img.maze, 0, 0, 224, 256, 0, 0, WIDTH, HEIGHT);
+}
+
+function renderDots() {
+	ctx.fillStyle = "#aaf";
+	for (var i = 0; i < map.length; i++) {
+		if (dots[i] === 1) {
+			ctx.fillRect(i%mapWidth*scale + scale/2, Math.floor(i/mapWidth)*scale + scale/2, 2, 2);
+		}
+	}
+}
+
