688127f4b0d4853943285837ec90fea3df49fab5
ljhobson <louishobson5@gmail.com>
2023-12-03 20:15:37 +1300

    asdfkjhasfd

--- epicshirtmaker/script.js
+++ epicshirtmaker/script.js
@@ -7,12 +7,15 @@ function fitCanvas() {
 	canvas.height = Math.min(window.innerHeight-50, 500);
 }
 
-function drawShirt(shirt, x, y, preview) {
+function drawShirt(shirt, x, y, preview, colScale) {
 	if (shirt.length == 0) {
 		return;
 	}
 	if (preview === undefined) {
 		preview = 0;
+	}
+	if (colScale === undefined) {
+		colScale = 1;
 	}
 	
 	if (preview) { // draw back
@@ -35,7 +38,7 @@ function drawShirt(shirt, x, y, preview) {
 			ctx.lineTo(x + shirt[i][0], y + shirt[i][1]);
 		}
 		
-		var grd = ctx.createLinearGradient(x, y-70, x, y-30);
+		var grd = ctx.createLinearGradient(x, y-70*colScale, x, y-30*colScale);
 		grd.addColorStop(0, "#b09990");
 		grd.addColorStop(1, "#a09089");
 
@@ -67,7 +70,7 @@ function drawShirt(shirt, x, y, preview) {
 		ctx.lineTo(x - shirt[i][0], y + shirt[i][1]);
 	}
 	if (preview) {
-		var grd = ctx.createLinearGradient(x-50, y, x+50, y);
+		var grd = ctx.createLinearGradient(x-50*colScale, y, x+50*colScale, y);
 		grd.addColorStop(0, "#dcb");
 		grd.addColorStop(0.25, "#ddb");
 		grd.addColorStop(0.5, "#dcb");
@@ -84,58 +87,60 @@ function drawShirt(shirt, x, y, preview) {
 		ctx.beginPath();
 		ctx.lineWidth = 1;
 		for (var i = topMost; i > 0; i--) {
+			var vec1 = addVecs(1, shirt[i], -1, shirt[i-1]);
+			var vec2 = addVecs(1, shirt[i], -1, shirt[i+1]);
 			
-			var vec1 = addVecs(1, shirt[i-1], -1, shirt[i]);
-			var vec2 = addVecs(1, shirt[i+1], -1, shirt[i]);
+			var offset1 = [-vec1[1], vec1[0]];
+			var offset2 = [vec2[1], -vec2[0]];
 			
-			vec1 = addVecs(1/vecSize(vec1), vec1, 0, [0, 0]);
-			vec2 = addVecs(1/vecSize(vec2), vec2, 0, [0, 0]);
+			offset1 = addVecs(neckWidth/vecSize(offset1), offset1, 0, [0, 0]);
+			offset2 = addVecs(neckWidth/vecSize(offset2), offset2, 0, [0, 0]);
 			
-			var dot = dotProd(vec1, vec2);
-			var det = determ(vec1, vec2);
-			var angle = Math.atan2(det, dot);
+			vec1 = addVecs(neckWidth/vecSize(vec1), vec1, 0, [0, 0]);
+			vec2 = addVecs(neckWidth/vecSize(vec2), vec2, 0, [0, 0]);
 			
-			console.log(i, angle);
-			var newPoint;
-			if (angle < 0) {
-				newPoint = addVecs(1, vec1, 1, vec2);
-			} else {
-				newPoint = addVecs(-1, vec1, -1, vec2);
+			var t = (offset2[0] - offset1[0]) / (vec1[0] - vec2[0]);
+			if (i === topMost) {
+				t = (offset2[0]) / (vec1[0] - vec2[0]);
 			}
+			var newPoint = addVecs(1, offset1, t, vec1);
 			
-			newPoint = addVecs(1, shirt[i], neckWidth/vecSize(newPoint), newPoint);
-			ctx.lineTo(x - newPoint[0], y + newPoint[1]);
+			ctx.lineTo(x - shirt[i][0] - newPoint[0], y + shirt[i][1] + newPoint[1]);
 		}
-		
 		// flat v neck end
 		ctx.lineTo(x - shirt[0][0] - neckWidth/2, y + shirt[0][1] + neckWidth);
 		ctx.lineTo(x - shirt[0][0] + neckWidth/2, y + shirt[0][1] + neckWidth);
 		
 		for (var i = 1; i <= topMost; i++) {
+			var vec1 = addVecs(1, shirt[i], -1, shirt[i-1]);
+			var vec2 = addVecs(1, shirt[i], -1, shirt[i+1]);
 			
-			var vec1 = addVecs(1, shirt[i-1], -1, shirt[i]);
-			var vec2 = addVecs(1, shirt[i+1], -1, shirt[i]);
+			var offset1 = [-vec1[1], vec1[0]];
+			var offset2 = [vec2[1], -vec2[0]];
 			
-			vec1 = addVecs(1/vecSize(vec1), vec1, 0, [0, 0]);
-			vec2 = addVecs(1/vecSize(vec2), vec2, 0, [0, 0]);
+			offset1 = addVecs(neckWidth/vecSize(offset1), offset1, 0, [0, 0]);
+			offset2 = addVecs(neckWidth/vecSize(offset2), offset2, 0, [0, 0]);
 			
-			var dot = dotProd(vec1, vec2);
-			var det = determ(vec1, vec2);
-			var angle = Math.atan2(det, dot);
+			vec1 = addVecs(neckWidth/vecSize(vec1), vec1, 0, [0, 0]);
+			vec2 = addVecs(neckWidth/vecSize(vec2), vec2, 0, [0, 0]);
 			
-			console.log(i, angle);
-			var newPoint;
-			if (angle < 0) {
-				newPoint = addVecs(1, vec1, 1, vec2);
-			} else {
-				newPoint = addVecs(-1, vec1, -1, vec2);
+			var t = (offset2[0] - offset1[0]) / (vec1[0] - vec2[0]);
+			var t = (offset2[0] - offset1[0]) / (vec1[0] - vec2[0]);
+			if (i === topMost) {
+				t = (offset2[0]) / (vec1[0] - vec2[0]);
+			}
+			var newPoint = addVecs(1, offset1, t, vec1);
+			
+			if (i === topMost) {
+				ctx.fillStyle = "#0a0";
+				//ctx.fillRect(x + shirt[i][0] + offset1[0], y + shirt[i][1] + offset1[1], 3, 3);
+				//ctx.fillRect(x + shirt[i][0] + offset2[0], y + shirt[i][1] + offset2[1], 3, 3);
+				
+				ctx.fillStyle = "#f00";
+				//ctx.fillRect(x + shirt[i][0] + newPoint[0] - 2, y + shirt[i][1] + newPoint[1] - 2, 3, 3);
 			}
 			
-			newPoint = addVecs(1, shirt[i], neckWidth/vecSize(newPoint), newPoint);
-			ctx.lineTo(x + newPoint[0], y + newPoint[1]);
-			
-			//ctx.fillStyle = "#f00";
-			//ctx.fillRect(x + newPoint[0], y + newPoint[1], 4, 4);
+			ctx.lineTo(x + shirt[i][0] + newPoint[0], y + shirt[i][1] + newPoint[1]);
 		}
 		ctx.stroke();
 	}
@@ -143,15 +148,15 @@ function drawShirt(shirt, x, y, preview) {
 
 function render(preview) {
 	fitCanvas();
-	
-	var scale = 1 + Number(preview === true);
+	preview = true;
+	var scale = 1 + 1*Number(preview === true);
 	
 	var previewShirt = [];
 	for (var i = 0; i < shirt.length; i++) {
 		previewShirt.push([scale * shirt[i][0], scale * shirt[i][1]]);
 	}
 	
-	drawShirt(previewShirt, canvas.width/2, canvas.height/2, preview);
+	drawShirt(previewShirt, canvas.width/2, canvas.height/2, preview, scale);
 }
 
 
